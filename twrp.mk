0e50c1165f40cf0673d37f8d2790a685bec3e6ee
xiaolvmu <mg@ecake.in>
2015-05-02 18:44:28 +0800

    Give a try of Android L-fix boot
    Change-Id: I9fc771d1c4ec7e6c9a6afcb42ec1ac7070a829d2

--- twrp.mk
+++ twrp.mk
@@ -1,21 +1,10 @@
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
-
-# Recovery:Start
-
-# Use this flag if the board has a ext4 partition larger than 2gb
-BOARD_HAS_LARGE_FILESYSTEM := true
-
-# Recovery: set depending on recovery being built for. (CWM or TWRP)
-#           both init scripts can be found in the recovery folder
-#TARGET_RECOVERY_INITRC := device/htc/villec2/recovery/init-twrp.rc
-
-PRODUCT_COPY_FILES += device/htc/villec2/recovery/etc/twrp.fstab:recovery/root/etc/twrp.fstab
-#SELinux
-HAVE_SELINUX := true
-
-DEVICE_RESOLUTION := 540x960
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+# Twrp
+TW_THEME := portrait_mdpi
+TW_INCLUDE_CRYPTO := true
+TW_NO_SCREEN_BLANK := true
 TW_INCLUDE_DUMLOCK := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-BOARD_HAS_NO_REAL_SDCARD := true
+HAVE_SELINUX := true
+TW_NO_CPU_TEMP := true
+TW_NEW_ION_HEAP := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
